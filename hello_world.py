800225e978222f1ab16778d631b88a1bbd6b5b26
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-07-30 16:14:14 +0100

    implemented additional string methods

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,16 @@
-print('Hello world!')
+my_string = "Hello world"
+my_string2 = "      Hello world       "
+my_string_upper = "Hello world".upper()
+my_string_lower = "Hello world".lower()
+my_string_title = "Hello world".title()
+my_string_split = "Hello world".split()
+my_string2_stripped = "      Hello world       ".strip()
+my_string_replaced = my_string.replace("Hello", "Goodbye")
+
+print(my_string)
+print(my_string_upper)
+print(my_string_lower)
+print(my_string_title)
+print(my_string_split)
+print(my_string2_stripped)
+print(my_string_replaced)
